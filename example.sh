2cb1b2944e59f1335c67461ac2a0f394989d1343
Rumman Ahmed <rahmed@rarserver.rar.com>
2020-03-21 15:27:05 -0500

    added the example script

---
+++ example.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+name=tea
+#Example hash
+echo "The word $name has ${#name} chars"
+echo $(( 2#111 ))
+
+#Example semicolon
+echo "hey there"; echo "you there"
+number=$1
+if [ "$number" -gt 0 ]; then echo "YES"; else echo "NO"; fi
+	
+
